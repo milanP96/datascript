a1fdc14175617fe246891f311ea9f3fe05fbbfaa
Milan Petkovic <milan96petkovic@gmail.com>
2023-12-13 13:07:40 +0100

    push

--- main.py
+++ main.py
@@ -1,44 +1,89 @@
 import requests
+from typing import List
+
+from models import Event, Loan
 
 
-PRICES = {
-    "ETH": 2200,
-    "wstETH": 2200,
-    "USD": 40000,
-    "BTC": 40000,
-    "DAI": 1,
-    "USDC": 1,
-    "USDT": 1
-}
+class StartScanSDK:
+    def __init__(self, contract_address: str):
+        self.contract_address = contract_address
+        self.events: List[Event] = list()
+        self.state_of_loans = dict()
+        self.assets = {}
+        self.asset_tokens = set()
 
+    def fetch_events(self):
+        """Fetch all data just by using pagination, next_url in response
+        put limit 100 to work with more data
+        """
+        url = f"https://api.starkscan.co/api/v0/events?from_address={self.contract_address}&limit=100"
 
-class Loan:
+        headers = {
+            "accept": "application/json",
+            "x-api-key": "docs-starkscan-co-api-3"
+        }
 
-    def update_state(self):
-        # TODO implementation
-        """update based on deposit, withdraw, borrow and repay"""
-        pass
+        response = requests.get(url, headers=headers)
 
-    def health(self):
-        # TODO implementation
-        """ calculating health (will be in the zklend docs)"""
-        pass
+        if response.status_code == 200:
+            res = response.json().get('data', [])
 
+            for item in res:
+                if item.get('key_name') in [
+                    "zklend::market::Market::Borrowing", "zklend::market::Market::Repay",
+                    "zklend::market::Market::Deposit", "zklend::market::Market::Withdrawal"
+                ]:
+                    event = Event.from_json(item)
+                    self.events.append(event)
 
-def fetch_events():
-    """Fetch all data just by using pagination, next_url in response"""
-    url = "https://api.starkscan.co/api/v0/events?from_address=0x04c0a5193d58f74fbace4b74dcf65481e734ed1714121bdc571da345540efa05"
+                    self.asset_tokens.add(event.token)
+        else:
+            raise Exception("Something went wrong")
 
-    headers = {
-        "accept": "application/json",
-        "x-api-key": "docs-starkscan-co-api-3"
-    }
+        for token in self.asset_tokens:
+            self.get_asset(token)
 
-    response = requests.get(url, headers=headers)
+        for event in self.events:
+            event.set_asset(
+                self.assets[event.token]
+            )
 
-    if response.status_code == 200:
-        return response.json().get('data', [])
+    def get_asset(self, token):
+        url = f"https://api.starkscan.co/api/v0/contract/{token}"
+
+        headers = {
+            "accept": "application/json",
+            "x-api-key": "docs-starkscan-co-api-3"
+        }
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            self.assets[token] = response.json()['name_tag'].split(" ")[1]
+        else:
+            raise Exception("alksdla")
+
+    def build_loans(self):
+
+        for event in self.events:
+
+            if event.user in self.state_of_loans:
+                self.state_of_loans[event.user].update_state(event=event)
+            else:
+                self.state_of_loans[event.user] = Loan(event=event)
 
 
 if __name__ == '__main__':
-    print(fetch_events())
+    sdk = StartScanSDK(contract_address="0x04c0a5193d58f74fbace4b74dcf65481e734ed1714121bdc571da345540efa05")
+    sdk.fetch_events()
+    sdk.build_loans()
+
+    for loan in sdk.state_of_loans:
+        print(f"""
+        Loan {loan} :
+        State in $: {sdk.state_of_loans[loan].sum_state()}
+        State per asset: 
+            Deposit - {dict(sdk.state_of_loans[loan].deposits)} 
+            Borrowed - {dict(sdk.state_of_loans[loan].borrowed)}
+        """)
+
